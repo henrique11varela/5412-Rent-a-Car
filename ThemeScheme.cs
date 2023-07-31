93de3a2746d26f5be93d6b1b789494135c9bb05d
henrique11varela <henrique11varela@gmail.com>
2023-07-31 19:10:35 +0100

    Added a style hub class

---
+++ ThemeScheme.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_a_Car
+{
+    internal static class ThemeScheme
+    {
+        //normal colors
+        public static Color primary = Color.FromArgb(0, 123, 255);
+        public static Color secondary = Color.FromArgb(108, 117, 125);
+        public static Color success = Color.FromArgb(40, 167, 69);
+        public static Color danger = Color.FromArgb(220, 53, 69);
+        public static Color warning = Color.FromArgb(255, 193, 7);
+        public static Color info = Color.FromArgb(23, 162, 184);
+        public static Color light = Color.FromArgb(248, 249, 250);
+        public static Color dark = Color.FromArgb(52, 58, 64);
+        public static Color muted = Color.FromArgb(108, 117, 125);
+        public static Color white = Color.FromArgb(255, 255, 255);
+        public static Color primary_emphasis = Color.FromArgb(110, 168, 254);
+        public static Color secondary_emphasis = Color.FromArgb(167, 172, 177);
+        public static Color success_emphasis = Color.FromArgb(117, 183, 152);
+        public static Color danger_emphasis = Color.FromArgb(234, 134, 143);
+        public static Color warning_emphasis = Color.FromArgb(255, 218, 106);
+        public static Color info_emphasis = Color.FromArgb(110, 223, 246);
+        public static Color dark_emphasis = Color.FromArgb(222, 226, 230);
+        public static Font mediumFont = new Font("Segoe UI", 10);
+        public static Font largeFont = new Font("Segoe UI", 16);
+
+    }
+}
