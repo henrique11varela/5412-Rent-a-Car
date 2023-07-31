c6d21d71cae985cb89441f6ce4f456fcae174eef
henrique11varela <henrique11varela@gmail.com>
2023-07-31 01:55:05 +0100

    Refactured front end
    - table is a component
    - changed file name

---
+++ Components/Tables/VehicleTable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent_a_Car.Components.Tables
+{
+    internal static class VehicleTable
+    {
+        public static void Setup(Panel view, int margin_x) {
+            //Constants
+
+            ///daratgridview and respective cells creation
+            DataGridView dgv = new DataGridView();
+
+            //grid and cells stylling
+            dgv.Location = new Point(margin_x, view.Top * 2);
+            dgv.Size = new Size(view.Width - margin_x * 2, 350);
+            dgv.BackgroundColor = Color.White;
+
+            //columns:
+            //id, marca, modelo, matricula, status (icon), tipo, details, edit
+            dgv.ColumnCount = 8;
+            dgv.RowCount = 15;
+
+            dgv.Columns[0].Name = "Id";
+            dgv.Columns[1].Name = "Marca";
+            dgv.Columns[2].Name = "Modelo";
+            dgv.Columns[3].Name = "Matricula";
+            dgv.Columns[4].Name = "Status";
+            dgv.Columns[5].Name = "Type";
+
+            dgv.RowHeadersVisible = false;
+            dgv.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            view.Controls.Add(dgv);
+        }
+    }
+}
---
+++ Views/VehicleList.cs
@@ -0,0 +1,68 @@
+﻿using Rent_a_Car.Components.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataGridView = System.Windows.Forms.DataGridView;
+
+namespace Rent_a_Car.Views
+{
+    internal class vehicleList
+    {
+        /// <summary>Adds elements to view[0].</summary>
+        public static void Setup(Button tab, Panel view)
+        {
+            //Name
+            tab.Text = "Vehicle List";
+
+            //Content
+            Font mediumFont = new Font("Segoe UI", 10);
+            Font largeFont = new Font("Segoe UI", 16);
+
+
+
+            ///label creation and styling
+            Label pageTitle = new Label();
+            pageTitle.Text = "List of Vehicles";
+            pageTitle.TextAlign = ContentAlignment.TopCenter;
+            pageTitle.AutoSize = true;
+            pageTitle.Location = new Point((view.Width - pageTitle.Width) / 2, 0);   //(widthWindow - widthLabel) / 2
+            pageTitle.Font = largeFont;
+
+
+
+            //Insert Table
+            VehicleTable.Setup(view, 25);
+
+
+
+
+
+            ///button with label
+
+            Button newVehicle = new Button();
+            newVehicle.Location = new Point(550, 40);
+            newVehicle.Name = "newVehicle";
+            newVehicle.Size = new Size(150, 30);
+            newVehicle.Text = "Add Vehicle          |  +  ";
+            newVehicle.TextAlign = ContentAlignment.MiddleLeft;
+            newVehicle.Font = mediumFont;
+
+            static void newVehicleClick(object sender, EventArgs e)
+            {
+
+            }
+
+
+
+
+
+
+            //Add content to view
+            view.Controls.Add(newVehicle);
+            view.Controls.Add(pageTitle);
+        }
+    }
+}
