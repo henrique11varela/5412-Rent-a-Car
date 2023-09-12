051918e27a31b61041556faf5286657847b2cc90
Deadfaced <btteixeira.bt@gmail.com>
2023-09-12 12:34:53 +0100

    Camiao details: finished output for Camiao details

--- Components/Details/CamiaoDetails.cs
+++ Components/Details/CamiaoDetails.cs
@@ -66,16 +66,61 @@ namespace Rent_a_Car.Components.Details
 
             int id = camiao.Id;
 
+            DetailBox marca = new DetailBox();
+            marca.label.Text = "Marca";
+            marca.textBox.Text = camiao.Marca;
+            this.Controls.Add(marca);
+            marca.Size = new Size(this.Width / 2 - (25 * 2), marca.Height);
+            marca.Location = new Point(25, 25);
 
-            //marca.textBox.Text = camiao.Marca;
-            //modelo.textBox.Text = camiao.Modelo;
-            //cor.textBox.Text = camiao.Cor;
-            //matricula.textBox.Text = camiao.Matricula;
-            //ano.textBox.Text = camiao.Ano.ToString();
-            //quantRodas.textBox.Text = camiao.QuantRodas.ToString();
-            //valorDia.textBox.Text = camiao.ValorDia.ToString();
-            //quantDoors.textBox.Text = camiao.QuantDoors.ToString();
-            //isManual.textBox.Text = camiao.IsManual.ToString();
+            DetailBox modelo = new DetailBox();
+            modelo.label.Text = "Modelo";
+            modelo.textBox.Text = camiao.Modelo;
+            this.Controls.Add(modelo);
+            modelo.Size = new Size(this.Width / 2 - (25 * 2), modelo.Height);
+            modelo.Location = new Point(this.Width / 2 + 25, 25);
+
+            DetailBox cor = new DetailBox();
+            cor.label.Text = "Cor";
+            cor.textBox.Text = camiao.Cor;
+            this.Controls.Add(cor);
+            cor.Size = new Size(this.Width / 2 - (25 * 2), cor.Height);
+            cor.Location = new Point(25, 25 + (25 + cor.Height) * 1);
+
+            DetailBox matricula = new DetailBox();
+            matricula.label.Text = "Matrícula";
+            matricula.textBox.Text = camiao.Matricula.ToString();
+            this.Controls.Add(matricula);
+            matricula.Size = new Size(this.Width / 2 - (25 * 2), matricula.Height);
+            matricula.Location = new Point(this.Width / 2 + 25, 25 + (25 + matricula.Height) * 1);
+
+            DetailBox ano = new DetailBox();
+            ano.label.Text = "Ano";
+            ano.textBox.Text = camiao.Ano.ToString();
+            this.Controls.Add(ano);
+            ano.Size = new Size(this.Width / 2 - (25 * 2), ano.Height);
+            ano.Location = new Point(25, 25 + (25 + ano.Height) * 2);
+
+            DetailBox quantRodas = new DetailBox();
+            quantRodas.label.Text = "Quantidade de Rodas";
+            quantRodas.textBox.Text = camiao.QuantRodas.ToString();
+            this.Controls.Add(quantRodas);
+            quantRodas.Size = new Size(this.Width / 2 - (25 * 2), quantRodas.Height);
+            quantRodas.Location = new Point(this.Width / 2 + 25, 25 + (25 + quantRodas.Height) * 2);
+
+            DetailBox valorDia = new DetailBox();
+            valorDia.label.Text = "Valor Dia";
+            valorDia.textBox.Text = camiao.ValorDia.ToString();
+            this.Controls.Add(valorDia);
+            valorDia.Size = new Size(this.Width / 2 - (25 * 2), valorDia.Height);
+            valorDia.Location = new Point(25, 25 + (25 + valorDia.Height) * 3);
+
+            DetailBox maxWeight = new DetailBox();
+            maxWeight.label.Text = "Peso Máximo";
+            maxWeight.textBox.Text = camiao.MaxWeight.ToString();
+            this.Controls.Add(maxWeight);
+            maxWeight.Size = new Size(this.Width / 2 - (25 * 2), maxWeight.Height);
+            maxWeight.Location = new Point(this.Width / 2 + 25, 25 + (25 + maxWeight.Height) * 3);
 
 
             //Reservar Button
